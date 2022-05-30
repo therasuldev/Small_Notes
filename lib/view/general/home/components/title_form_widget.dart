daaa2f833ecc83cb594b7c298cc8b701b8b601b2
therasuldev <rasul.ramixanov@gmail.com>
2022-05-30 17:52:04 +0400

    titleForm

--- lib/view/general/home/components/title_form_widget.dart
+++ lib/view/general/home/components/title_form_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smallnotes/view/constant/size_constant.dart';
+import 'package:smallnotes/view/constant/app_size.dart';
 import 'package:smallnotes/view/widgets/utils.dart';
 import 'package:smallnotes/view/widgets/widgets.dart';
 
@@ -28,7 +28,6 @@ class TitleForm extends NoteStatelessWidget {
         toolbarOptions: ViewUtils.toolbarOptions(),
         autofocus: true,
         focusNode: focusNode,
-        // onChanged: onChanged,
       ),
     );
   }
