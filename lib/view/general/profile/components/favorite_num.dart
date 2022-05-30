135f0423556e13e6ae2998bdff264cb4bea5a565
therasuldev <rasul.ramixanov@gmail.com>
2022-05-30 17:49:58 +0400

    favorite num

--- lib/view/general/profile/components/favorite_num.dart
+++ lib/view/general/profile/components/favorite_num.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smallnotes/view/constant/color_constant.dart';
+import 'package:smallnotes/view/constant/app_color.dart';
 import 'package:smallnotes/view/widgets/utils.dart';
 import 'package:smallnotes/view/widgets/widgets.dart';
 
@@ -9,17 +9,20 @@ class FavoriteNum extends NoteStatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      width: 66,
-      decoration: ViewUtils.decorationForProfAppBar(AppColors.red),
-      alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.favorite, color: AppColors.red),
-          Text('$favoriteNum', style: TextStyle(color: AppColors.red))
-        ],
+    return GestureDetector(
+      onTap: () =>  Navigator.pushNamed(context, '/favoritePG'),
+      child: Container(
+        height: 55,
+        width: 66,
+        decoration: ViewUtils.decorationForProfAppBar(AppColors.red),
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.favorite, color: AppColors.red),
+            Text('$favoriteNum', style: TextStyle(color: AppColors.red))
+          ],
+        ),
       ),
     );
   }
