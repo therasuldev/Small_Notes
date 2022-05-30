792a60188d24195e8e89f9d0bbc7ba539115ea63
therasuldev <rasul.ramixanov@gmail.com>
2022-05-30 17:55:55 +0400

    created new Favorite database

--- lib/core/database/notes_database.dart
+++ lib/core/database/notes_database.dart
@@ -2,14 +2,20 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
-  static const note = "NotesOfficial";
-
+  static const note = "theroshh";
+  static const favorite = "theroshhh";
   static Future<Database> database() async {
     final dbPath = await getDatabasesPath();
     return await openDatabase(
-      join(dbPath, "NotesOfficial"),
+      join(dbPath, "theroshhhh"),
       onCreate: (db, version) {
         db.execute("CREATE TABLE IF NOT EXISTS $note(id TEXT PRIMARY KEY ,"
+            " titleNote TEXT,"
+            " dateCreate TEXT,"
+            " textColor INTEGER,"
+            " backgroundColor INTEGER,"
+            " textNote TEXT)");
+        db.execute("CREATE TABLE IF NOT EXISTS $favorite(id TEXT PRIMARY KEY ,"
             " titleNote TEXT,"
             " dateCreate TEXT,"
             " textColor INTEGER,"
@@ -57,14 +63,14 @@ class DBHelper {
   static Future selectNoteById(String id) async {
     final db = await DBHelper.database();
     return await db.rawQuery(
-      "SELECT * from ${DBHelper.note} where id = ? ",
+      "SELECT * from ${DBHelper.note} where id = ? ", //! For DBHelper.note
       [id],
     );
   }
 
-  static Future<List<Map<String, dynamic>>> selectNote() async {
+  static Future<List<Map<String, dynamic>>> selectNote(String name) async {
     final db = await DBHelper.database();
-    var select = await db.query(DBHelper.note);
+    var select = await db.query(name);
     return select;
   }
 }
