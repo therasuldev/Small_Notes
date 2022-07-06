4725e21a6b3af21d68c7f87ed2dd68b9662284b5
therasuldev <rasul.ramixanov@gmail.com>
2022-07-06 17:06:28 +0400

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:smallnotes/app.dart';
 import 'package:smallnotes/core/app/intl.dart';
 import 'package:smallnotes/core/app/note.dart';
+import 'package:smallnotes/core/service/preference_service.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   final Note note = Note();
+
+  await Hive.initFlutter();
+  await Hive.openBox('allNotes');
+  await Hive.openBox('favorites');
+  await Hive.openBox('preferences');
+
   note.intl = Intl();
+  note.prefService = PrefService();
   note.intl.locale = const Locale('az');
   note.intl.supportedLocales = languages;
 
